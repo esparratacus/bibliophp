e9eee07005f3ca49e6f163d2ce772ee7383ccc84
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2018-05-30 01:28:24 -0500

    Implementando menu

---
+++ views/layout/foot.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guerrerojosedario
+ * Date: 2018/05/29
+ * Time: 9:56 PM
+ */
---
+++ views/layout/scripts.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guerrerojosedario
+ * Date: 2018/05/30
+ * Time: 12:42 AM
+ */
